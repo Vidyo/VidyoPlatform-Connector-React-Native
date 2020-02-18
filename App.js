cad636fb525b2ca7475d4b60e2ee07e3b08b5604
tmelko-vidyo <tmelko@vidyo.com>
2020-02-18 15:25:28 +0200

    + main project/packages files

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import AppNavigator from './Navigation/AppNavigator';
+
+export default class App extends Component {
+  render() {
+    return <AppNavigator />;
+  }
+}
