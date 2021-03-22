0247a932ffbef3a28aab3d457fde84729707c1ce
Live Memories <live.memories.info@gmail.com>
2021-03-22 14:12:32 +0200

    Upgrade: - React native: 0.63.4 - Swap Navigation stack impl.

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import AppNavigator from './Navigation/AppNavigator';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./Page/Home";
+import Conference from "./Page/Conference";
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
-    return <AppNavigator />;
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={"Home"}>
+          <Stack.Screen
+            name={"Home"}
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name={"Conference"}
+            component={Conference}
+            options={{
+              /* Show the header on the conference */
+              headerShown: true,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
 }
